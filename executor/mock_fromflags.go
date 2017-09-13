turbinelabs/adminserver

"upgrade" gomock to latest (fixes #3746)
The latest version of gomock appears to have broken API comparability; any new install of `mock/mockgen` will produce code that can't be run by the vendored `mock/gomock`. So that's neat.

This change upgrades the vendored gomock and re-generates all the code with the new mockgen.

This will require a `make go-update-tools` for it to work on anyone else's machine.

Once nice pickup is that comments will now lint properly.

fbshipit-source-id: 2bebb0f

mock_fromflags.go
@@ -1,50 +1,58 @@
-// Automatically generated by MockGen. DO NOT EDIT!
+// Code generated by MockGen. DO NOT EDIT.
 // Source: fromflags.go
 
+// Package adminserver is a generated GoMock package.
 package adminserver
 
 import (
 	gomock "github.com/golang/mock/gomock"
 	proc "github.com/turbinelabs/nonstdlib/proc"
+	reflect "reflect"
 )
 
-// Mock of FromFlags interface
+// MockFromFlags is a mock of FromFlags interface
 type MockFromFlags struct {
 	ctrl     *gomock.Controller
-	recorder *_MockFromFlagsRecorder
+	recorder *MockFromFlagsMockRecorder
 }
 
-// Recorder for MockFromFlags (not exported)
-type _MockFromFlagsRecorder struct {
+// MockFromFlagsMockRecorder is the mock recorder for MockFromFlags
+type MockFromFlagsMockRecorder struct {
 	mock *MockFromFlags
 }
 
+// NewMockFromFlags creates a new mock instance
 func NewMockFromFlags(ctrl *gomock.Controller) *MockFromFlags {
 	mock := &MockFromFlags{ctrl: ctrl}
-	mock.recorder = &_MockFromFlagsRecorder{mock}
+	mock.recorder = &MockFromFlagsMockRecorder{mock}
 	return mock
 }
 
-func (_m *MockFromFlags) EXPECT() *_MockFromFlagsRecorder {
-	return _m.recorder
+// EXPECT returns an object that allows the caller to indicate expected use
+func (m *MockFromFlags) EXPECT() *MockFromFlagsMockRecorder {
+	return m.recorder
 }
 
-func (_m *MockFromFlags) Validate() error {
-	ret := _m.ctrl.Call(_m, "Validate")
+// Validate mocks base method
+func (m *MockFromFlags) Validate() error {
+	ret := m.ctrl.Call(m, "Validate")
 	ret0, _ := ret[0].(error)
 	return ret0
 }
 
-func (_mr *_MockFromFlagsRecorder) Validate() *gomock.Call {
-	return _mr.mock.ctrl.RecordCall(_mr.mock, "Validate")
+// Validate indicates an expected call of Validate
+func (mr *MockFromFlagsMockRecorder) Validate() *gomock.Call {
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Validate", reflect.TypeOf((*MockFromFlags)(nil).Validate))
 }
 
-func (_m *MockFromFlags) Make(managedProc proc.ManagedProc) AdminServer {
-	ret := _m.ctrl.Call(_m, "Make", managedProc)
+// Make mocks base method
+func (m *MockFromFlags) Make(managedProc proc.ManagedProc) AdminServer {
+	ret := m.ctrl.Call(m, "Make", managedProc)
 	ret0, _ := ret[0].(AdminServer)
 	return ret0
 }
 
-func (_mr *_MockFromFlagsRecorder) Make(arg0 interface{}) *gomock.Call {
-	return _mr.mock.ctrl.RecordCall(_mr.mock, "Make", arg0)
+// Make indicates an expected call of Make
+func (mr *MockFromFlagsMockRecorder) Make(managedProc interface{}) *gomock.Call {
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Make", reflect.TypeOf((*MockFromFlags)(nil).Make), managedProc)
 }

turbinelabs/nonstdlib

executor/mock_fromflags.go
@@ -1,40 +1,46 @@
-// Automatically generated by MockGen. DO NOT EDIT!
+// Code generated by MockGen. DO NOT EDIT.
 // Source: fromflags.go
 
+// Package executor is a generated GoMock package.
 package executor
 
 import (
 	gomock "github.com/golang/mock/gomock"
 	log "log"
+	reflect "reflect"
 )
 
-// Mock of FromFlags interface
+// MockFromFlags is a mock of FromFlags interface
 type MockFromFlags struct {
 	ctrl     *gomock.Controller
-	recorder *_MockFromFlagsRecorder
+	recorder *MockFromFlagsMockRecorder
 }
 
-// Recorder for MockFromFlags (not exported)
-type _MockFromFlagsRecorder struct {
+// MockFromFlagsMockRecorder is the mock recorder for MockFromFlags
+type MockFromFlagsMockRecorder struct {
 	mock *MockFromFlags
 }
 
+// NewMockFromFlags creates a new mock instance
 func NewMockFromFlags(ctrl *gomock.Controller) *MockFromFlags {
 	mock := &MockFromFlags{ctrl: ctrl}
-	mock.recorder = &_MockFromFlagsRecorder{mock}
+	mock.recorder = &MockFromFlagsMockRecorder{mock}
 	return mock
 }
 
-func (_m *MockFromFlags) EXPECT() *_MockFromFlagsRecorder {
-	return _m.recorder
+// EXPECT returns an object that allows the caller to indicate expected use
+func (m *MockFromFlags) EXPECT() *MockFromFlagsMockRecorder {
+	return m.recorder
 }
 
-func (_m *MockFromFlags) Make(_param0 *log.Logger) Executor {
-	ret := _m.ctrl.Call(_m, "Make", _param0)
+// Make mocks base method
+func (m *MockFromFlags) Make(arg0 *log.Logger) Executor {
+	ret := m.ctrl.Call(m, "Make", arg0)
 	ret0, _ := ret[0].(Executor)
 	return ret0
 }
 
-func (_mr *_MockFromFlagsRecorder) Make(arg0 interface{}) *gomock.Call {
-	return _mr.mock.ctrl.RecordCall(_mr.mock, "Make", arg0)
+// Make indicates an expected call of Make
+func (mr *MockFromFlagsMockRecorder) Make(arg0 interface{}) *gomock.Call {
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Make", reflect.TypeOf((*MockFromFlags)(nil).Make), arg0)
 }